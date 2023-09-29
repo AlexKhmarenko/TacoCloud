b873653b744a454f64c12ec2ab47030bc6b3a699
Oleksandr Khmarenko <khmarenko.a@gmail.com>
2023-09-29 22:31:29 +0300

    Commit P120

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -2,6 +2,7 @@ package tacos.data;
 
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
+
 public interface OrderRepository
     extends CrudRepository<TacoOrder, Long> {
 }
